e2cac588b35e08f737a11b9533037fab5da878f8
Francis Dolan <francis.dolan@gmail.com>
2024-09-22 13:47:20 +0100

    Updated seed file

--- repo/testdata/default-seeds.sql
+++ repo/testdata/default-seeds.sql
@@ -1,7 +1,8 @@
+TRUNCATE TABLE jobs;
+TRUNCATE TABLE requests;
 INSERT INTO `requests` (`token`, `request_token`, `action`, `params`, `extras`, `steps`, `step`, `status`, `created`, `completed`)
 VALUES
-	('lala', 'hoho', 'delete', '{\"id\": 1}', '{}', '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service1GetUser\", \"service1GetUser\"], \"name\": \"service2_delete_user\"}]', 0, 0, '2024-09-19 12:05:19', NULL),
-	('lala2', 'hoho2', 'delete2', '{\"id\": 2}', '{}', '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service1GetUser\", \"service1GetUser\"], \"name\": \"service2_delete_user\"}]', 0, 0, '2024-09-19 12:05:41', NULL),
-	('lala3', 'hoho3', 'delete3', '{\"id\": 3}', '{}', '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service1GetUser\", \"service1GetUser\"], \"name\": \"service2_delete_user\"}]', 0, 1, '2024-09-19 12:05:35', NULL),
-	('lala4', 'hoho4', 'delete4', '{\"id\": 4}', '{}', '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service1GetUser\", \"service1GetUser\"], \"name\": \"service2_delete_user\"}]', 0, 6, '2024-09-19 12:05:47', NULL);
-
+	('lala', 'hoho', 'delete', '{\"id\": 1}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 0, '2024-09-09 14:23:21', NULL),
+	('lala2', 'hoho2', 'delete2', '{\"id\": 2}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 0, '2024-09-09 14:23:26', NULL),
+	('lala3', 'hoho3', 'delete3', '{\"id\": 3}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 1, '2024-09-09 14:23:30', NULL),
+	('lala4', 'hoho4', 'delete4', '{\"id\": 4}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 6, '2024-09-09 14:23:34', NULL);
