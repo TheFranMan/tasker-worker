53a98aa3cbac266d5fff6fe1b65672d47b50fc36
Francis Dolan <francis.dolan@gmail.com>
2024-09-28 15:24:00 +0100

    Updated default seeder

--- repo/testdata/default-seeds.sql
+++ repo/testdata/default-seeds.sql
@@ -2,7 +2,6 @@ TRUNCATE TABLE jobs;
 TRUNCATE TABLE requests;
 INSERT INTO `requests` (`token`, `request_token`, `action`, `params`, `extras`, `steps`, `step`, `status`, `created`, `completed`)
 VALUES
-	('lala', 'hoho', 'delete', '{\"id\": 1}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 0, '2024-09-09 14:23:21', NULL),
-	('lala2', 'hoho2', 'delete2', '{\"id\": 2}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 0, '2024-09-09 14:23:26', NULL),
-	('lala3', 'hoho3', 'delete3', '{\"id\": 3}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 1, '2024-09-09 14:23:30', NULL),
-	('lala4', 'hoho4', 'delete4', '{\"id\": 4}', "{}", '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service2DeleteUser\", \"service1DeleteUser\"], \"name\": \"service2_delete_user\"}]', 0, 6, '2024-09-09 14:23:34', NULL);
+	('test-token-1', 'auth-token-1', 'Delete', '{\"id\": 1}', '{}', '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service1DeleteUser\", \"service2DeleteUser\", \"service3DeleteUser\"], \"name\": \"delete_user_accounts\"}]', 0, 0, '2024-09-28 14:14:27', NULL),
+	('test-token-2', 'auth-token-2', 'Delete', '{\"id\": 2}', '{}', '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service1DeleteUser\", \"service2DeleteUser\", \"service3DeleteUser\"], \"name\": \"delete_user_accounts\"}]', 0, 0, '2024-09-28 14:16:54', NULL),
+	('test-token-3', 'auth-token-3', 'Delete', '{\"id\": 4}', '{}', '[{\"jobs\": [\"service1GetUser\"], \"name\": \"service1_retrieve_user\"}, {\"jobs\": [\"service1DeleteUser\", \"service2DeleteUser\", \"service3DeleteUser\"], \"name\": \"delete_user_accounts\"}]', 0, 6, '2024-09-28 14:17:01', NULL);
