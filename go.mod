7306db1ba95830688a847e271a211913cc296fcc
Francis Dolan <francis.dolan@gmail.com>
2024-10-02 15:39:28 +0100

    Added intergration test structure for wrkRequests intergarion tests

--- go.mod
+++ go.mod
@@ -15,10 +15,28 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/containerd/continuity v0.4.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/huandu/xstrings v1.4.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/opencontainers/runc v1.1.14 // indirect
+	github.com/ory/dockertest v3.3.5+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ wrkRequests_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jmoiron/sqlx"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
+	"github.com/stretchr/testify/suite"
+)
+
+type Suite struct {
+	suite.Suite
+	db       *sqlx.DB
+	pool     *dockertest.Pool
+	resource *dockertest.Resource
+}
+
+func TestRun(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
+
+func (s *Suite) SetupSuite() {
+	var err error
+
+	s.pool, err = dockertest.NewPool("")
+	if nil != err {
+		s.FailNowf(err.Error(), "cannot create a new dockertest pool")
+	}
+
+	err = s.pool.Client.Ping()
+	if nil != err {
+		s.FailNowf(err.Error(), "cannot ping dockertest client")
+	}
+
+	s.resource, err = s.pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "mysql",
+		Tag:        "8.0",
+		Env: []string{
+			"MYSQL_ROOT_PASSWORD=secret",
+		},
+	}, func(config *docker.HostConfig) {
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	if nil != err {
+		s.FailNowf(err.Error(), "cannot create dockertest mysql s.resource")
+	}
+
+	s.resource.Expire(60 * 1)
+
+	err = s.pool.Retry(func() error {
+		var err error
+		s.db, err = sqlx.Open("mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql", s.resource.GetPort("3306/tcp")))
+		if err != nil {
+			return err
+		}
+
+		return s.db.Ping()
+	})
+	if nil != err {
+		s.FailNowf(err.Error(), "cannot open dockertest mysql connection")
+	}
+
+	// Migrations
+	driver, err := mysql.WithInstance(s.db.DB, &mysql.Config{})
+	if nil != err {
+		s.FailNowf(err.Error(), "cannot create migration driver:")
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"mysql",
+		driver,
+	)
+	if nil != err {
+		s.FailNowf(err.Error(), "cannot create new migration instance")
+	}
+
+	err = m.Up()
+	if nil != err {
+		s.FailNowf(err.Error(), "cannot run mysql migrations")
+	}
+}
+
+func (s *Suite) TearDownSuite() {
+	// err := s.pool.Purge(s.resource)
+	// if nil != err {
+	// 	s.FailNowf(err.Error(), "cannot purge dockertest mysql resource")
+	// }
+}
+
+func (s *Suite) AfterTest() {
+	s.T().Log("In Aftertest")
+}
+
+func (s *Suite) Test_new_requests_are_marked_in_progress_and_their_first_step_jobs_inserted() {
+	s.T().Log("In test1")
+
+}
+
+func (s *Suite) Test_if_the_request_is_new__insert_the_first_step_jobs_and_update_the_request_status_as_in_progress() {
+	s.T().Log("In test2")
+
+}
+
+func (s *Suite) Test_if_the_request_has_an_errored_job_reinsert_the_job_and_do_not_update_the_request_status() {
+	s.T().Log("In test3")
+
+}
+
+func (s *Suite) Test_if_a_successfull_step_is_not_the_requests_last_step_increment_its_step_and_insert_the_new_steps_jobs() {
+	s.T().Log("In test4")
+
+}
+
+func (s *Suite) Test_if_a_successfull_step_is_the_requests_last_step_update_the_requests_status_as_completed() {
+	s.T().Log("In test5")
+
+}
